ccabb87cf600e29755aa021c84f071c223729b32
Nicolas Nueca <nicolasnueca@Nicolass-MacBook-Air.local>
2023-08-02 17:06:37 -0700

    generating stage model

---
+++ migrations/20230802184833-create-stage.js
@@ -0,0 +1,28 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('stage', {
+      stage_id: {
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      stage_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      capacity: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('stage');
+  }
+};
