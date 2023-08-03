d5d3777d18ff812404f1b238ad642b6ea7c5cdc2
Nicolas Nueca <nicolasnueca@Nicolass-MacBook-Air.local>
2023-08-02 17:17:14 -0700

    cleaning code

--- migrations/20230802184833-create-stage.js
+++ migrations/20230802184833-create-stage.js
@@ -8,10 +8,6 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      stage_id: {
-        type: Sequelize.INTEGER,
-        allowNull: false
-      },
       name: {
         type: Sequelize.STRING,
         allowNull: false
