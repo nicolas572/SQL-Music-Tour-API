2a73f21e26092d81a54bbdb4a65238a1a203fe0b
nicolas572 <nicolas572@yahoo.com>
2024-03-12 19:38:44 -0700

    updating Band Show Route

--- controllers/bands_controller.js
+++ controllers/bands_controller.js
@@ -21,10 +21,10 @@ bands.get('/', async (req, res) => {
 })
 
 // FIND A SPECIFIC BAND
-bands.get('/:id', async (req, res) => {
+bands.get('/:name', async (req, res) => {
     try {
         const foundBand = await Band.findOne({
-            where: { band_id: req.params.id }
+            where: { name: req.params.name }
         })
         res.status(200).json(foundBand)
     } catch (error) {
